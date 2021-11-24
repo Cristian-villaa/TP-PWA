39617631fd5d96c03b0e5811e40e9394fd30d83e
Cristian <cristianporras.v@gmail.com>
2021-11-24 14:34:15 -0300

    6) Implementar la clase IdentityInterface

--- src/basic/models/Usuario.php
+++ src/basic/models/Usuario.php
@@ -15,7 +15,7 @@ use Yii;
  * @property int $accessToken
  * @property int $authkey
  */
-class Usuario extends \yii\db\ActiveRecord
+class Usuario extends \yii\db\ActiveRecord implements \yii\web\IdentityInterface
 {
     /**
      * {@inheritdoc}
@@ -31,7 +31,7 @@ class Usuario extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['username', 'nombre', 'apellido', 'password', 'accessToken', 'authkey'], 'required'],
+            [['username', 'nombre', 'apellido', 'password'], 'required'],
             [['password', 'accessToken', 'authkey'], 'integer'],
             [['username'], 'string', 'max' => 50],
             [['nombre', 'apellido'], 'string', 'max' => 100],
@@ -52,5 +52,36 @@ class Usuario extends \yii\db\ActiveRecord
             'accessToken' => 'Access Token',
             'authkey' => 'Authkey',
         ];
+    }
+    //Nuevos metodos implementados 
+    public static function findIdentity($id){
+        return self::findOne($id);
+    } 
+
+    public static function findIdentityByAccessToken($token, $type = null)
+    {
+        return self::findOne(['accessToken'=>$token]);
+    } 
+
+    public static function findByUsername($username){
+        return self::findOne(['username'=>$username]);
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+    public function getAuthKey()
+    {
+        return $this->authkey;
+    }
+
+    public function validateAuthKey($authKey)
+    {
+        return $this->authKey === $authKey;
+    }
+    public function validatePassword($password){
+
+        return password_verify($password, $this->password);    
     }
 }
